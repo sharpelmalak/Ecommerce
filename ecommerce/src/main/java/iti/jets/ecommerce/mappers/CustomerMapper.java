452217fdc7acec98ff1f0a23b63b8bd4cc5253bc
SharpelMalak <sharbelmalak@gmail.com>
2024-10-22 19:24:37 +0300

    add swagger dep

---
+++ ecommerce/src/main/java/iti/jets/ecommerce/mappers/CustomerMapper.java
@@ -0,0 +1,10 @@
+package iti.jets.ecommerce.mappers;
+
+import iti.jets.ecommerce.dto.CustomerDTO;
+import iti.jets.ecommerce.models.Customer;
+
+public class CustomerMapper {
+
+
+}
+
---
+++ ecommerce/src/main/java/iti/jets/ecommerce/services/CustomerService.java
@@ -0,0 +1,10 @@
+package iti.jets.ecommerce.services;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+
+}
