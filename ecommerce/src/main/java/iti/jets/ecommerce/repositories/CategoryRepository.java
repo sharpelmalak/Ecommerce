7305ace836c55556db00c9db1088ac413b9712b6
Dwidar <ahmed.hamdy1421999@gmail.com>
2024-10-22 21:49:33 +0300

    merge conflict

--- ecommerce/src/main/java/iti/jets/ecommerce/repositories/CategoryRepository.java
+++ ecommerce/src/main/java/iti/jets/ecommerce/repositories/CategoryRepository.java
@@ -5,8 +5,6 @@ import iti.jets.ecommerce.models.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
-
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
