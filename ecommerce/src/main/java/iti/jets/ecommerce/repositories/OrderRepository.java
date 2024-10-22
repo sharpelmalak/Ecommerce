ce3cbefc59ecd32acc6b4a234c2597f591c605d2
Mohamad-Khalid <mohamad.khalid2021@gmail.com>
2024-10-22 19:37:09 +0300

    add order, orderItem in [models, repos, service,controller]

---
+++ ecommerce/src/main/java/iti/jets/ecommerce/repositories/OrderRepository.java
@@ -0,0 +1,7 @@
+package iti.jets.ecommerce.repositories;
+
+import iti.jets.ecommerce.models.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+}
