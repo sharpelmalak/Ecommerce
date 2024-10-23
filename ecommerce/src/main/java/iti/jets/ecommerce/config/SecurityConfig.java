bf19057b1554ece548a2aafb96cc63ea1a0600e2
SharpelMalak <sharbelmalak@gmail.com>
2024-10-23 18:31:58 +0300

    jwtFilter and service

--- ecommerce/src/main/java/iti/jets/ecommerce/config/SecurityConfig.java
+++ ecommerce/src/main/java/iti/jets/ecommerce/config/SecurityConfig.java
@@ -4,21 +4,28 @@ import iti.jets.ecommerce.services.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
 
     @Autowired
     private CustomUserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtFilter jwtFilter;
 
     @Bean
     public AuthenticationProvider authenticationProvider() {
@@ -33,17 +40,24 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable()) // Disable CSRF protection
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**","/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                        .requestMatchers("/api/auth/login","/api/auth/register","/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         // Allow access to authentication endpoints
-                        .requestMatchers("/**").permitAll()
+                       // .requestMatchers("/**").permitAll()
                        // .requestMatchers("/api/admin/**").hasRole("ADMIN") // Restrict access to admin endpoints
                         .anyRequest().authenticated() // Other endpoints require authentication
                 )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 //.formLogin(Customizer.withDefaults())
         ; // Optionally, configure form login if needed
 
