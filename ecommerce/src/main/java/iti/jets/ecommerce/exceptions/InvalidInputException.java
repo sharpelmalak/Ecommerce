d9da2920fec6cbfe592e173dc330b0ba471f14d5
Mohamad-Khalid <mohamad.khalid2021@gmail.com>
2024-11-14 15:41:49 +0300

    add Backend Validation on edit customer address data

---
+++ ecommerce/src/main/java/iti/jets/ecommerce/exceptions/InvalidInputException.java
@@ -0,0 +1,7 @@
+package iti.jets.ecommerce.exceptions;
+
+public class InvalidInputException extends RuntimeException {
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
